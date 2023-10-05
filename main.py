d68885d11fd7d5aabab231e8ee78cdaf483a015b
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-05 09:40:45 +0300

    int, float, comparisons

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
 print("Hello, world!")
 print("From VS Code - Python")
 print("Today is 20231005")
+
+# result = input("How are you today?")
+# if result == "Good":
+#     print ("Great to hear that you are Good!")
+# else:
+#     print ("Sorry to hear that you are not Good!")
+
+# This is a comment
+"""
+    This is a multi-line comment
+    That continues across the lines
+"""
+
+i = 5 #int
+print(type(i))
+
+j = 3.41 #float
+print(type(j))
+
+intsum = 1 + 2
+if intsum == 3:
+    print("Yay, I can do int addition!")
+else:
+    print("This is an int surprise!")
+
+floatsum = 0.1 + 0.2
+if floatsum == 0.3:
+    print("Yay, I can do float addition!")
+else:
+    print("This is a float surprise!")
+
+if abs(floatsum - 0.3) < 0.000001:
+    print("Yay, I can do accurate enough float addition!")
+else:
+    print("This is a big float surprise!")
