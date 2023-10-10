7620b419d9f3987663cde36c2d1d136d41d03ffa
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-10 09:16:11 +0300

    if, elif, else, match

--- main.py
+++ main.py
@@ -1,123 +1,26 @@
-print("Hello, world!")
-print("From VS Code - Python")
-print("Today is 20231005")
 
-# result = input("How are you today?")
-# if result == "Good":
-#     print ("Great to hear that you are Good!")
+# if condition:
+#     true statement
+# elif:
+#     another true statement
 # else:
-#     print ("Sorry to hear that you are not Good!")
+#     false statement
 
-# This is a comment
-"""
-    This is a multi-line comment
-    That continues across the lines
-"""
+# match starting with Python 3.10
 
-i = 5 #int
-print(type(i))
+i = "a"
 
-j = 3.41 #float
-print(type(j))
-
-intsum = 1 + 2
-if intsum == 3:
-    print("Yay, I can do int addition!")
+if i == "a":
+    pass    # handle case 1
+elif i == "b":
+    pass # handle case 2
+elif i == "c":
+    pass # handle case 3
 else:
-    print("This is an int surprise!")
+    pass # default case
 
-floatsum = 0.1 + 0.2
-if floatsum == 0.3:
-    print("Yay, I can do float addition!")
-else:
-    print("This is a float surprise!")
-
-if abs(floatsum - 0.3) < 0.000001:
-    print("Yay, I can do accurate enough float addition!")
-else:
-    print("This is a big float surprise!")
-
-b = True
-print(type(b))
-notB = False
-
-# Conversions
-f = 3.9536356
-print(f)
-print(type(f))
-
-intF = int(f)
-print(intF)
-print(type(intF))
-
-# dynamic types in Python
-# variable types can "change"
-f = int(f)
-print(f)
-print(type(f))
-
-int_list = [3, 5, 6]
-print(int_list)
-print(type(int_list))
-
-mixed_list = [3, 5.535, "a string"]
-print(mixed_list)
-print(type(mixed_list))
-
-int_tuple = (3, 5, 6)
-print(int_tuple)
-print(type(int_tuple))
-
-mixed_tuple = (3, 5.535, "a string")
-print(mixed_tuple)
-print(type(mixed_tuple))
-
-# modifications / mutation
-print("Before modification", int_list)
-int_list[1] = 242.545
-print("After modification", int_list)
-
-# compile error
-# wrong_list = ["fsdfsf")
-
-print("Before modification", int_tuple)
-# runtime error
-# int_tuple[1] = 242.545
-print("After modification", int_tuple)
-
-# set
-list_duplicates = [3, 5, 9, 3, 5]
-print(list_duplicates)
-
-int_set = {3, 5, 9, 3, 5}
-print(int_set)
-int_set.add(13)
-print(int_set)
-int_set.add(3)
-print(int_set)
-
-if 4 in int_set:
-    print("4 exists in set")
-else:
-    print("4 does not exist in set")
-
-# helpful to eliminate duplicates
-print("with duplicates ", list_duplicates)
-list_duplicates = list(set(list_duplicates))
-print("no duplicates ", list_duplicates)
-
-# dictionary
-class_dict = {
-    "IE 201": "Caner Taşkın",
-    "IE 255": "Aybek Korugan",
-    "IE 202": "Tınaz Ekim"
-}
-
-print ("IE 201 is taught by: ", class_dict["IE 201"])
-
-if "IE 203" in class_dict:
-    print ("IE 203 is taught by: ", class_dict["IE 203"])
-else:
-    print ("IE 203 is not taught this semester")
-
-print (class_dict)
+match i:
+    case "a": pass # handle case 1
+    case "b": pass # handle case 2
+    case "c": pass # handle case 3
+    case _: pass # default case 
