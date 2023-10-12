c847b1f84dfc8d3d48cfd57a9879c7fbc592ee78
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-12 09:49:30 +0300

    arbitrary arguments

--- main.py
+++ main.py
@@ -15,11 +15,38 @@ my_function("caner", age)
 print(f"Value of age after function fall {age}")
 
 # optional parameters and default values
-def foo(i, j, k = 50):
-    print(f"i = {i} j = {j} k = {k}")
+# i and j are positional parameters
+# k and l are keyword parameters
+def foo(i, j, k = 50, l = 100):
+    print(f"i = {i} j = {j} k = {k} l = {l}")
 
 foo (10, 15)
 foo (10, 15, 40)
 
 # next line gives an error since we don't have all required parameters
 # foo (10)
+
+foo (10, 15, l=80)
+
+foo (10, 15, k = 70, l=80)
+
+def sum(i, j, k = 0):
+    return i + j + k
+
+result = sum (4, 6)
+print(result)
+
+result2 = sum (4, 6, 10)
+print(result2)
+
+# arbitrary arguments are treated as a tuple
+def sum_all(*inputs):
+    total = 0
+    for i in inputs:
+        total += i
+    return total
+
+sum0 = sum_all()
+sum1 = sum_all(1)
+sum2 = sum_all(4, 5)
+sum3 = sum_all(4, 5, 19)
