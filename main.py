e7aeb9a6af84ff59da6bd316dd1757acb2f4b675
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-12 09:35:00 +0300

    optional parameters, default values

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import time
 
 # function parameters are local variables
 # global variables can be defined but not a good idea
-def my_function(name):
-    global age
+def my_function(name, age):
+    # global age
     age = 15
 
     print(f"Hello {name} I am my_function")
@@ -11,5 +11,15 @@ def my_function(name):
 
 age = 12
 print(f"Value of age before function fall {age}")
-my_function("caner")
+my_function("caner", age)
 print(f"Value of age after function fall {age}")
+
+# optional parameters and default values
+def foo(i, j, k = 50):
+    print(f"i = {i} j = {j} k = {k}")
+
+foo (10, 15)
+foo (10, 15, 40)
+
+# next line gives an error since we don't have all required parameters
+# foo (10)
