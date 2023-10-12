7e7b4e3cb379f866f64f8b3243157c60ac6c34ab
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-12 10:41:04 +0300

    factorial iterative vs recursive

--- main.py
+++ main.py
@@ -1,52 +1,22 @@
-import time
+def factorial(n):
+    result = 1
+    current = 1
+    while current <= n:
+        result *= current
+        current += 1
+    return result
 
-# function parameters are local variables
-# global variables can be defined but not a good idea
-def my_function(name, age):
-    # global age
-    age = 15
+def factorial_recursive(n):
+    if n == 0:
+        return 1
+    return n * factorial_recursive(n-1)
 
-    print(f"Hello {name} I am my_function")
-    print(f"Good to know that you are {age} years old")
+fact5 = factorial(5)
+print(f"5! = {fact5} calculated iteratively")
 
-age = 12
-print(f"Value of age before function fall {age}")
-my_function("caner", age)
-print(f"Value of age after function fall {age}")
+recursive_fact5 = factorial_recursive(5)
+print(f"5! = {recursive_fact5} calculated recursively")
 
-# optional parameters and default values
-# i and j are positional parameters
-# k and l are keyword parameters
-def foo(i, j, k = 50, l = 100):
-    print(f"i = {i} j = {j} k = {k} l = {l}")
-
-foo (10, 15)
-foo (10, 15, 40)
-
-# next line gives an error since we don't have all required parameters
-# foo (10)
-
-foo (10, 15, l=80)
-
-foo (10, 15, k = 70, l=80)
-
-def sum(i, j, k = 0):
-    return i + j + k
-
-result = sum (4, 6)
-print(result)
-
-result2 = sum (4, 6, 10)
-print(result2)
-
-# arbitrary arguments are treated as a tuple
-def sum_all(*inputs):
-    total = 0
-    for i in inputs:
-        total += i
-    return total
-
-sum0 = sum_all()
-sum1 = sum_all(1)
-sum2 = sum_all(4, 5)
-sum3 = sum_all(4, 5, 19)
+# mind the maximum recursion depth 
+# recursive_fact1000 = factorial_recursive(1000)
+# print(f"1000! = {recursive_fact1000} calculated recursively")
