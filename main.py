5d0412349fbafa4f3cc0b52c66212111187b692f
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-12 09:23:14 +0300

    local variable

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import time
 
-def my_function():
-    print("Hello from my_function")
+# function parameters are local variables
+def my_function(name, age):
+    age = 15
+    print(f"Hello {name} I am my_function")
+    print(f"Good to know that you are {age} years old")
 
-my_function()
+age = 12
+print(f"Value of age before function fall {age}")
+my_function("caner", age)
+print(f"Value of age after function fall {age}")
