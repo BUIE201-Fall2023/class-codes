c7a86fcb98654bc7a1163d33228ccbdb2fecc70a
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-12 09:27:56 +0300

    global variables

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import time
 
 # function parameters are local variables
-def my_function(name, age):
+# global variables can be defined but not a good idea
+def my_function(name):
+    global age
     age = 15
+
     print(f"Hello {name} I am my_function")
     print(f"Good to know that you are {age} years old")
 
 age = 12
 print(f"Value of age before function fall {age}")
-my_function("caner", age)
+my_function("caner")
 print(f"Value of age after function fall {age}")
