9f4aedb3a0ea64516d1564c05698ec2e82f89c3d
Deniz Dağlar <denizekindaglar@hotmail.com>
2023-10-17 11:34:22 +0300

    added tail_rec

--- main.py
+++ main.py
@@ -38,15 +38,29 @@ def fibonacci_memoized(n, previous_values):
 previous_value = {}
 fib5memoized = fibonacci_memoized(5, previous_value)
 
-n = 40
+def fibonacci_tail_rec(n,a=0,b=1):
+    if n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        return fibonacci_tail_rec(n-1, b, a+b)
+# seems to be about the same speed or faster than memoized
+
+
+n = 500
 start_iterative = time.time()
 fibonacci_iterative(n)
 print(f"fibonacci_iterative({n}) took {time.time() - start_iterative}")
 
-start_recursive = time.time()
-fibonacci_recursive(n)
-print(f"fibonacci_recursive({n}) took {time.time() - start_recursive}")
+#start_recursive = time.time()
+#fibonacci_recursive(n)
+#print(f"fibonacci_recursive({n}) took {time.time() - start_recursive}")
 
 start_memoized = time.time()
 fibonacci_memoized(n, {})
 print(f"fibonacci_memoized({n}) took {time.time() - start_memoized}")
+
+start_tail_rec = time.time()
+fibonacci_tail_rec(n)
+print(f"fibonacci_tail_rec({n}) took {time.time() - start_tail_rec}")
