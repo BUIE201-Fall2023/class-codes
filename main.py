b2ca432f2d1e25a00ab804ad7f54356e35029866
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-19 10:28:01 +0300

    set_date

--- main.py
+++ main.py
@@ -16,11 +16,38 @@ class Date:
 
     def print(self):
         print(f"The date is {self.Day}/{self.Month}/{self.Year}")
+    
+    def set_date(self, Year, Month, Day):
+        if not (Month >= 1 and Month <= 12):
+            print("Invalid month")
+            return False 
+        if not (Day >= 1 and Day <= 31):
+            print("Invalid day")
+            return False 
+        ### Add other validations
+        self.Year = Year
+        self.Month = Month
+        self.Day = Day
+        return True
 
 today = Date()
-today.Year = 2023
-today.Month = 10
-today.Day = 19
+print("today: ", id(today))
+# violates data encapsulation / data hiding principles
+# today.Year = 2023
+# today.Month = 10
+# today.Day = 19
+
+today.set_date(2023, 10, -19)
+print("today: ", id(today))
+
+today.set_date(2023, 10, 19)
+print("today: ", id(today))
+
 today.print()
+
+tomorrow = Date()
+print("tomorrow: ", id(tomorrow))
+tomorrow.set_date(2023, 10, 20)
+tomorrow.print()
 
 i = 4
