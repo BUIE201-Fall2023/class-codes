2d7daee208a5fbadc989d34c916804a4cbc60d4f
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-24 09:24:05 +0300

    multiple references to the same object

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 
-Year = 2023
-Month = 10
-Day = 19
-
-def print_date(Year, Month, Day):
-    print(f"The date is {Day}/{Month}/{Year}")
-
-print_date(Year, Month, Day)
-
 class Date:
     def __init__(self, Year, Month, Day) -> None:
         self.Year = 1970
         self.Month = 1
         self.Day = 1
         self.set_date(Year, Month, Day)
-
-        # class attributes are shared across all objects
-        class_attribute = 5   
 
     def print(self):
         print(f"The date is {self.Day}/{self.Month}/{self.Year}")
@@ -34,24 +22,14 @@ class Date:
         self.Day = Day
         return True
 
-today = Date(2023, 10, -19)
+today = Date(2023, 10, 24)
 print("today: ", id(today))
-# violates data encapsulation / data hiding principles
-# today.Year = 2023
-# today.Month = 10
-# today.Day = 19
-
-today.set_date(2023, 10, -19)
-print("today: ", id(today))
-
-today.set_date(2023, 10, 19)
-print("today: ", id(today))
-
 today.print()
 
-tomorrow = Date(2023, 10, 20)
+tomorrow = Date(2023, 10, 25)
 print("tomorrow: ", id(tomorrow))
-# tomorrow.set_date(2023, 10, 20)
 tomorrow.print()
 
-i = 4
+tomorrow = today
+print("tomorrow: ", id(tomorrow))
+tomorrow.print()
