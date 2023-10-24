f2710057dc73fc8cf1f966cd0b3b7e99fa23d5d8
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-24 09:06:19 +0300

    validation and initialization parameters

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ def print_date(Year, Month, Day):
 print_date(Year, Month, Day)
 
 class Date:
-    def __init__(self) -> None:
+    def __init__(self, Year, Month, Day) -> None:
         self.Year = 1970
         self.Month = 1
         self.Day = 1
+        self.set_date(Year, Month, Day)
+
+        # class attributes are shared across all objects
+        class_attribute = 5   
 
     def print(self):
         print(f"The date is {self.Day}/{self.Month}/{self.Year}")
@@ -30,7 +34,7 @@ class Date:
         self.Day = Day
         return True
 
-today = Date()
+today = Date(2023, 10, -19)
 print("today: ", id(today))
 # violates data encapsulation / data hiding principles
 # today.Year = 2023
@@ -45,9 +49,9 @@ print("today: ", id(today))
 
 today.print()
 
-tomorrow = Date()
+tomorrow = Date(2023, 10, 20)
 print("tomorrow: ", id(tomorrow))
-tomorrow.set_date(2023, 10, 20)
+# tomorrow.set_date(2023, 10, 20)
 tomorrow.print()
 
 i = 4
