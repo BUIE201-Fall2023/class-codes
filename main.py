79589f09a4993df1832dd41e8a278922b779b8f8
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-10-24 12:08:51 +0300

    mutable & immutable

--- main.py
+++ main.py
@@ -37,8 +37,8 @@ tomorrow.print()
 def f(date):
     print(type(date), id(date))
     date.print()
-    # date.set_date(2025, 10, 24)
-    date = Date(2025, 10, 24)
+    date.set_date(2025, 10, 24)
+    # date = Date(2025, 10, 24)
     print(type(date), id(date))
     date.print()
 
@@ -47,4 +47,28 @@ today.print()
 f(today)
 print("today after f: ", id(today))
 today.print()
+
+# int objects are immutable
+# https://www.guru99.com/mutable-and-immutable-in-python.html
+def g(i):
+    print(type(i), id(i), i)
+    i += 5
+    print(type(i), id(i), i)
+
+j = 10
+print("before g: ", type(j), id(j), j)
+g(j)
+print("after g: ", type(j), id(j), j)
+
+my_dictionary = {"IE 201": "Caner Taşkın"}
+
+# dict works with immutable key types
+# my_dictionary2 = {["IE", "201"] : "Caner Taşkın"}
+
+# everything is an object in Python
+my_float = 3.14
+print(type(my_float), id(my_float), my_float)
+my_float.is_integer()
+my_str = f"the value of my_float is {my_float}"
+my_str = "the value of my_float is {}".format(my_float)
 
