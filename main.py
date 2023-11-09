642977e402797908ea8b52649e77431246de8e22
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-11-09 09:43:46 +0300

    issubclass

--- main.py
+++ main.py
@@ -25,3 +25,25 @@ b = Bird()
 pets = [c, d, b]
 for pet in pets:
     pet.talk()
+
+i = 5
+print(type(i)) #int
+
+my_bool = True
+print(type(my_bool)) #bool
+print("issubclass(bool, int)", issubclass(bool, int))
+
+my_float = 3.41
+print(type(my_float)) #float
+print("issubclass(int, float)", issubclass(int, float))
+
+my_int = int(my_float)
+
+print(type(d)) #dog
+print(type(b)) #bird
+
+print("issubclass(Bird, Animal)", issubclass(Bird, Animal))
+
+print("issubclass(int, object)", issubclass(int, object))
+print("issubclass(float, object)", issubclass(float, object))
+print("issubclass(Animal, object)", issubclass(Animal, object))
