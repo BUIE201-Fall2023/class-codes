8ddf36589e137cfae02e669727c5efe6bcb03629
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-11-30 10:10:43 +0300

    insert

--- main.py
+++ main.py
@@ -1,49 +1,27 @@
-class Animal:
-    def talk(self):
-        pass
+class Array:
+    def __init__(self) -> None:
+        self.size = 0
+        self.array = None
+    
+    def insert(self, value):
+        # allocate new array
+        new_size = self.size + 1
+        new_array = [None] * new_size
 
-class Cat(Animal):
-    def talk(self):
-        print("miyav")
+        # copy existing elements to the new array
+        for i in range(self.size):
+            new_array[i] = self.array[i]
 
-class Dog(Animal):
-    def talk(self):
-        print("hav")
+        #insert the new item at the end
+        new_array[self.size] = value
 
-class Bird(Animal):
-    def talk(self):
-        print("cik")
+        # update attributes 
+        self.size = new_size
+        self.array = new_array
 
-c = Cat()
-c.talk()
 
-d = Dog()
-d.talk()
+my_array = Array()
+my_array.insert(3)
+my_array.insert(5)
+my_array.insert(6)
 
-# polymorphism
-b = Bird()
-pets = [c, d, b]
-for pet in pets:
-    pet.talk()
-
-i = 5
-print(type(i)) #int
-
-my_bool = True
-print(type(my_bool)) #bool
-print("issubclass(bool, int)", issubclass(bool, int))
-
-my_float = 3.41
-print(type(my_float)) #float
-print("issubclass(int, float)", issubclass(int, float))
-
-my_int = int(my_float)
-
-print(type(d)) #dog
-print(type(b)) #bird
-
-print("issubclass(Bird, Animal)", issubclass(Bird, Animal))
-
-print("issubclass(int, object)", issubclass(int, object))
-print("issubclass(float, object)", issubclass(float, object))
-print("issubclass(Animal, object)", issubclass(Animal, object))
