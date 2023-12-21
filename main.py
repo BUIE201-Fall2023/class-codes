b8807a7d37d120b2d490f38dce6597a94db4c570
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-12-21 10:49:49 +0300

    find, count

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ class TreeNode:
         self.left = None
         self.right = None
 
+    # O(log n)
     def insert_recursive(self, value):
         if value <= self.value:
             if self.left == None:
@@ -15,7 +16,31 @@ class TreeNode:
                 self.right = TreeNode(value)
             else:
                 self.right.insert_recursive(value)
+    
+    # O(log n)
+    def find_recursive(self, value):
+        if value == self.value:
+            return True
+        elif value < self.value:
+            if self.left == None:
+                return False
+            else:
+                return self.left.find_recursive(value)
+        else:
+            if self.right == None:
+                return False
+            else:
+                return self.right.find_recursive(value)
+    
+    def count_recursive(self):
+        left_count = 0
+        if self.left:
+            left_count = self.left.count_recursive()
+        right_count = 0
+        if self.right:
+            right_count = self.right.count_recursive()
 
+        return 1 + left_count + right_count
 class Tree:
     def __init__(self) -> None:
         self.root = None
@@ -27,6 +52,20 @@ class Tree:
         else:
             self.root.insert_recursive(value)
 
+    # O(log n)  
+    def find(self, value):
+        if self.root == None:
+            return False
+        else:
+            return self.root.find_recursive(value)
+
+    # O(n)  
+    def count(self):
+        if self.root == None:
+            return 0
+        else:
+            return self.root.count_recursive()
+
 t = Tree()
 t.insert(10)
 t.insert(15)
@@ -35,3 +74,9 @@ t.insert(13)
 t.insert(3)
 t.insert(8)
 
+r = t.find(8)
+r2 = t.find(20)
+
+n = t.count()
+
+i = 4
