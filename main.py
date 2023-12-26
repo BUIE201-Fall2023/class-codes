606cf55aa21915b897f1c1f5cb872fd78cb311d8
Z. Caner Taşkın <caner.taskin@icrontech.com>
2023-12-26 10:36:11 +0300

    hash

--- main.py
+++ main.py
@@ -1,98 +1,37 @@
-class TreeNode:
-    def __init__(self, value) -> None:
-        self.value = value
-        self.left = None
-        self.right = None
+i = hash("caner")
+print(i)
 
-    # O(log n)
-    def insert_recursive(self, value):
-        if value <= self.value:
-            if self.left == None:
-                self.left = TreeNode(value)
-            else:
-                self.left.insert_recursive(value)
-        else:
-            if self.right == None:
-                self.right = TreeNode(value)
-            else:
-                self.right.insert_recursive(value)
-    
-    # O(log n)
-    def find_recursive(self, value):
-        if value == self.value:
-            return True
-        elif value < self.value:
-            if self.left == None:
-                return False
-            else:
-                return self.left.find_recursive(value)
-        else:
-            if self.right == None:
-                return False
-            else:
-                return self.right.find_recursive(value)
-    
-    def count_recursive(self):
-        left_count = 0
-        if self.left:
-            left_count = self.left.count_recursive()
-        right_count = 0
-        if self.right:
-            right_count = self.right.count_recursive()
-
-        return 1 + left_count + right_count
-    
-    # O(n)
-    def print_recursive(self):
-        if self.left:
-            self.left.print_recursive()
-        print(self.value, " ", end="")
-        if self.right:
-            self.right.print_recursive()
-class Tree:
+class HashTable:
     def __init__(self) -> None:
-        self.root = None
-    
-    # O(log n)
-    def insert(self, value):
-        if self.root == None:
-            self.root = TreeNode(value)
-        else:
-            self.root.insert_recursive(value)
+        self.BUCKET_SIZE = 100
+        self.buckets = [None] * self.BUCKET_SIZE
 
-    # O(log n)  
+    # O(1) on average
+    def insert(self, value):
+        hashed_value = hash(value)
+        index = hashed_value % self.BUCKET_SIZE
+        if self.buckets[index] is None:
+            self.buckets[index] = []
+        self.buckets[index].append(value)
+    
+    # O(1) on average
     def find(self, value):
-        if self.root == None:
+        hashed_value = hash(value)
+        index = hashed_value % self.BUCKET_SIZE
+
+        if self.buckets[index] is None:
             return False
         else:
-            return self.root.find_recursive(value)
+            return value in self.buckets[index]
 
-    # O(n)  
-    def count(self):
-        if self.root == None:
-            return 0
-        else:
-            return self.root.count_recursive()
-    
-    def print(self):
-        print("{", end="")
-        if self.root:
-            self.root.print_recursive()
-        print("}")
+# Python set and dictionary are based on hash tables
+        
+h = HashTable()
+h.insert("caner")
+h.insert("tamer")
+h.insert("tınaz")
 
-t = Tree()
-t.insert(10)
-t.insert(15)
-t.insert(5)
-t.insert(13)
-t.insert(3)
-t.insert(8)
+find1 = h.find("caner")
+find2 = h.find("mustafa")
 
-r = t.find(8)
-r2 = t.find(20)
-
-n = t.count()
-
-t.print()
-
-i = 4
+i = 5
